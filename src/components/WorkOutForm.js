695a1fd1f6ec911d53ff82d21670ce0d47c42d05
LolaEnBeta <lolitaenbeta@gmail.com>
2019-08-19 18:48:15 +0200

    Complete WorkoutForm with inputs options

--- src/components/WorkOutForm.js
+++ src/components/WorkOutForm.js
@@ -1,18 +1,37 @@
 import React from 'react';
 
-class WorkOutForm extends React.Component {
+class WorkoutForm extends React.Component {
     render() {
         return (
             <div className="container">
-                <select name="workOut">
-                    <option value="">Select one</option>
-                    <option value="pushups">Pushups</option>
-                    <option value="abs">ABS</option>
-                </select>
+                <div>
+                    <select name="workout">
+                        <option value="">Select one</option>
+                        <option value="pushups">Pushups</option>
+                        <option value="abs">ABS</option>
+                    </select>
+                </div>
 
+                <label htmlFor="reps">How many repetitions did you do?</label>
+
+                <div>
+                    <input type="text" name="reps" />
+                </div>
+
+                <label htmlFor="totalTime">How many time did you working out?</label>
+
+                <div>
+                    <input type="text" name="totalTime" />
+                </div>
+
+                <label htmlFor="weight">How many weight did you use?</label>
+
+                <div>
+                    <input type="text" name="weight" />
+                </div>
             </div>
         );
     }
 }
 
-export default WorkOutForm;
+export default WorkoutForm;
