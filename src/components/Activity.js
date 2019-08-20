e55f4f0562d76cfec8e166610fc27ec3b109ab43
LolaEnBeta <lolitaenbeta@gmail.com>
2019-08-20 10:52:11 +0200

    Create structure of Activity.js component

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -3,7 +3,16 @@ import React from 'react';
 class Activity extends React.Component {
     render() {
         return(
-
+            <div className="card m-5">
+                <div className="card-header">
+                    Type
+                </div>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">Reps</li>
+                    <li className="list-group-item">Time</li>
+                    <li className="list-group-item">Weight</li>
+                </ul>
+            </div>
         );
     }
 }
