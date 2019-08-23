05ad607957845c2b5e6cbcae69f845f87f871019
LolaEnBeta <lolitaenbeta@gmail.com>
2019-08-23 11:25:28 +0200

    Put some style in Activity.js component

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -3,17 +3,31 @@ import React from 'react';
 class Activity extends React.Component {
     render() {
         return(
-            <div className="card m-5">
+            <div className="card m-5" style={{width: 250}}>
                 <div className="card-header">
                     {this.props.type}
                 </div>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item">Reps: {this.props.reps}</li>
-                    <li className="list-group-item">Total time: {this.props.totalTime}</li>
-                    <li className="list-group-item">Weight: {this.props.weight}</li>
-                    <li className="list-group-item">Id: {this.props.id}</li>
+                <ul className="list-group">
+                    <li className="list-group-item d-flex justify-content-between align-items-center">
+                        Reps:
+                        <span className="badge badge-primary badge-pill">{this.props.reps}</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between align-items-center">
+                        Total time:
+                        <span className="badge badge-primary badge-pill">{this.props.totalTime}</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between align-items-center">
+                        Weight:
+                        <span className="badge badge-primary badge-pill">{this.props.weight}</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between align-items-center">
+                        Id:
+                        <span className="badge badge-primary badge-pill">{this.props.id}</span>
+                    </li>
                 </ul>
-                <button onClick={this.deleteActivity.bind(this)}>
+                <button
+                    className="btn btn-danger"
+                    onClick={this.deleteActivity.bind(this)}>
                     Delete
                 </button>
             </div>
