60b6a8487eacdcf7b6363d195f2ea284a7635022
LolaEnBeta <lolitaenbeta@gmail.com>
2019-09-03 15:27:50 +0200

    Create a function to insert the activity in DB

--- src/ActivityRepository.py
+++ src/ActivityRepository.py
@@ -1,2 +1,27 @@
 import sqlite3
 from activity import Activity
+
+def crate_activity(activity):
+    conn = sqlite3.connect("sqlite3/database.db")
+    query = conn.cursor()
+
+    arguments = (
+        activity.type,
+        activity.reps,
+        activity.id_act,
+        activity.totalTime,
+        activity.weight
+    )
+
+    sql = '''
+        INSERT INTO activities (type, reps, id_act, totalTime, weight)
+        VALUES (?, ?, ?, ?, ?)
+    '''
+
+    if (query.execute(sql, arguments)):
+        query.close()
+        conn.commit()
+        conn.close()
+        return "Activity created successfully"
+    else:
+        return "An error has ocurred"
