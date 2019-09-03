1ec3ec76e3cd7863a3f81a895775b7ba23dbd18b
LolaEnBeta <lolitaenbeta@gmail.com>
2019-09-03 14:48:23 +0200

    Create a class Activity.py to use in the API

---
+++ src/activitiy.py
@@ -0,0 +1,7 @@
+class Activity(object):
+    def __init__(self, type, reps, id, totalTime="", weight=""):
+        self.type = type
+        self.reps = reps
+        self.id = id
+        self.totalTime = totalTime
+        self.weight = weight
