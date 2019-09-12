324c922d4d72326ecb0ab15929ca0a72df43d40c
LolaEnBeta <lolitaenbeta@gmail.com>
2019-09-12 15:47:08 +0200

    Create a Link in CalendarPage that open new page

--- src/routers/CalendarPage.js
+++ src/routers/CalendarPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Calendar from 'react-calendar';
+import { Link } from "react-router-dom";
 
 class CalendarPage extends React.Component {
   constructor() {
@@ -10,23 +11,26 @@ class CalendarPage extends React.Component {
   }
 
   render() {
-      const onChange = date => {
-        this.setState({
-          date: date
-        });
-      }
+    const onChange = date => {
+      this.setState({
+        date: date,
+      });
+    }
 
-      return (
-          <div className="container mt-5 ml-5">
-            <Calendar
-              onChange={onChange.bind(this)}
-              value={this.state.date}/>
-            <button
-              className="mt-4 btn btn-primary">
-              Add new activity
-            </button>
-          </div>
-      )
+    return (
+      <div className="container mt-5 ml-5">
+        <Calendar
+          onChange={onChange.bind(this)}
+          value={this.state.date}
+        />
+        <Link
+          className="btn btn-secondary mr-3"
+          to={"/day?day="}
+        >
+          See activities
+        </Link>
+      </div>
+    )
   }
 }
 
