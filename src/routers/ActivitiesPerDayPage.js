c94b3b709769f6ba4089ef30058b13e88367516d
LolaEnBeta <lolitaenbeta@gmail.com>
2019-09-17 11:41:42 +0200

    Remove demo-activities from Activity component

--- src/routers/ActivitiesPerDayPage.js
+++ src/routers/ActivitiesPerDayPage.js
@@ -2,89 +2,18 @@ import React from 'react';
 import Activity from '../components/Activity';
 import axios from 'axios';
 
-const demoData = {
-    "2019/09/12": [
-        {
-            "type": "abs",
-            "reps": 2,
-            "totalTime": 10,
-            "weight": 0,
-            "id": 1
-        },
-        {
-            "type": "salt",
-            "reps": 200,
-            "totalTime": 50,
-            "weight": 0,
-            "id": 2
-        }
-    ],
-    "2019/08/26": [
-        {
-            "type": "abs",
-            "reps": 30,
-            "totalTime": 0,
-            "weight": 0,
-            "id": 3
-        },
-        {
-            "type": "sqd",
-            "reps": 10,
-            "totalTime": 20,
-            "weight": 0,
-            "id": 4
-        }
-    ],
-    "2019/04/23": [
-        {
-            "type": "push",
-            "reps": 2,
-            "totalTime": 0,
-            "weight": 10,
-            "id": 5
-        },
-        {
-            "type": "tri",
-            "reps": 20,
-            "totalTime": 0,
-            "weight": 0,
-            "id": 6
-        },
-        {
-            "type": "salt",
-            "reps": 200,
-            "totalTime": 50,
-            "weight": 0,
-            "id": 7
-        },
-        {
-            "type": "sqd",
-            "reps": 10,
-            "totalTime": 20,
-            "weight": 0,
-            "id": 8
-        },{
-            "type": "sqd",
-            "reps": 10,
-            "totalTime": 20,
-            "weight": 0,
-            "id": 9
-        }
-    ]
-};
-
 class ActivitiesPerDayPage extends React.Component {
     constructor({ match, location }) {
         super();
         const params = new URLSearchParams(location.search);
         let day = params.get('day')
-        // let activitiesList = demoData[day]
 
         this.state = {
             date: day,
             activities: [],
         }
     }
+
     componentDidMount() {
         axios.get('http://127.0.0.1:5000/activities', {
             params: {
