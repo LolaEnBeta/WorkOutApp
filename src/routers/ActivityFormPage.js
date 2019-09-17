14e8fb3966c70dd06568844fb06552664a38a1e7
LolaEnBeta <lolitaenbeta@gmail.com>
2019-09-17 10:54:59 +0200

    Remove Activities from ActivityFormPage

--- src/routers/ActivityFormPage.js
+++ src/routers/ActivityFormPage.js
@@ -1,70 +1,21 @@
 import React from 'react';
-import ActivityForm from '../components/ActivityForm';
-import Activity from '../components/Activity';
-import axios from 'axios';
+import Form from '../components/Form';
 
 class ActivityFormPage extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-          activities: [],
-        }
+  constructor() {
+    super();
+    this.state = {
+      activities: [],
     }
-    componentDidMount() {
-        axios.get('http://127.0.0.1:5000/activities')
-          .then(res => {
-            const activities = res.data;
-            this.setState({ activities })
-        });
-    }
+  }
 
-    render() {
-        const createActivity = activity => {
-            let activities = this.state.activities;
-            activities.push(activity);
-
-            this.setState({
-              activities: activities,
-            })
-        }
-
-
-        async function removeActivity(id) {
-          let response = await axios ({
-              method: 'DELETE',
-              header: "Content-Type: application/json",
-              url: 'http://127.0.0.1:5000/activities/' + id,
-          });
-          if (response.status === 200) {
-            response = await axios.get('http://127.0.0.1:5000/activities')
-            const activities = response.data;
-            this.setState({ activities });
-          }
-        }
-
-        return (
-            <div className="container">
-                <ActivityForm
-                // onActivityCreated={createActivity.bind(this)}
-                />
-                <div>
-                    {this.state.activities.map(activity => {
-                        return (
-                        <Activity
-                            onActivityDeleted={removeActivity.bind(this)}
-                            key={activity.id}
-                            type={activity.type}
-                            reps={activity.reps}
-                            totalTime={activity.totalTime}
-                            weight={activity.weight}
-                            id={activity.id}
-                        />
-                        );}
-                    )}
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="container">
+        <Form />
+      </div>
+    );
+  }
 }
 
 export default ActivityFormPage;
