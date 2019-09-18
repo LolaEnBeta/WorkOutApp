911a4930e0790e06292d087cadbb8825145e7f4b
LolaEnBeta <lolitaenbeta@gmail.com>
2019-09-18 12:29:53 +0200

    Pass day params to axios in componentDidMount()

--- src/routers/Activities.js
+++ src/routers/Activities.js
@@ -6,40 +6,38 @@ class Activities extends React.Component {
     constructor() {
         super();
         this.state = {
+            date: "2019/09/17",
             activities: [],
         }
     }
 
     componentDidMount() {
-        axios.get('http://127.0.0.1:5000/activities')
-            .then(res => {
+        axios.get('http://127.0.0.1:5000/activities',{
+            params: {
+                day: this.state.date,
+            }
+        }).then(res => {
             const activities = res.data;
             this.setState({ activities })
         });
     }
 
-    render() {
+    async removeActivity(id) {
+        let response = await axios.delete('http://127.0.0.1:5000/activities/' + id);
 
-        async function removeActivity(id) {
-            let response = await axios ({
-                method: 'DELETE',
-                header: "Content-Type: application/json",
-                url: 'http://127.0.0.1:5000/activities/' + id,
-            });
-
-            if (response.status === 200) {
-                response = await axios.get('http://127.0.0.1:5000/activities')
-                const activities = response.data;
-                this.setState({ activities });
-            }
+        if (response.status === 200) {
+            response = await axios.get('http://127.0.0.1:5000/activities')
+            const activities = response.data;
+            this.setState({ activities });
         }
-
+    }
+    render() {
         return (
             <div className="container">
                 {this.state.activities.map(activity => {
                     return (
                         <Activity
-                            onActivityDeleted={removeActivity.bind(this)}
+                            onActivityDeleted={this.removeActivity.bind(this)}
                             key={activity.id}
                             type={activity.type}
                             reps={activity.reps}
