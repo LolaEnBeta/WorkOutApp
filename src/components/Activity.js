58f3bb1db40e6a5675810c7c6e233fd21a201db2
LolaEnBeta <lolitaenbeta@gmail.com>
2019-09-19 18:59:20 +0200

    Create a Link in activity to go to ModifyForm to modify it

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 class Activity extends React.Component {
 
@@ -38,7 +39,11 @@ class Activity extends React.Component {
                 </button>
                 <button
                     className="btn btn-danger">
-                    Modify
+                    <Link
+                        to={"/modifyform?id =" + this.props.id + "&type=" + this.props.type + "&reps=" + this.props.reps + "&totalTime=" + this.props.totalTime + "&weight=" + this.props.weight}
+                        >
+                        Modify
+                    </Link>
                 </button>
             </div>
         );
