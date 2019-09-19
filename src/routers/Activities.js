e80f2142f50bb33a047d8473f5d6af1c3c1c9e6b
LolaEnBeta <lolitaenbeta@gmail.com>
2019-09-19 16:00:19 +0200

    Put axios in changeStateValues to get the activities by type

--- src/routers/Activities.js
+++ src/routers/Activities.js
@@ -6,20 +6,23 @@ class Activities extends React.Component {
     constructor() {
         super();
         this.state = {
-            date: "2019/09/17",
+            type: '',
             activities: [],
         }
     }
 
     componentDidMount() {
-        axios.get('http://127.0.0.1:5000/activities',{
-            params: {
-                day: this.state.date,
-            }
-        }).then(res => {
-            const activities = res.data;
-            this.setState({ activities })
-        });
+        if (this.state.type) {
+            axios.get('http://127.0.0.1:5000/type', {
+                params: {
+                    type: this.state.type
+                }
+            })
+            .then(res => {
+                const activities = res.data;
+                this.setState({ activities })
+            });
+        }
     }
 
     async removeActivity(id) {
@@ -31,28 +34,45 @@ class Activities extends React.Component {
             this.setState({ activities });
         }
     }
+
+    changeStateValues ({name, value}) {
+
+        axios.get('http://127.0.0.1:5000/type', {
+            params: {
+                type: value
+            }
+        })
+        .then(res => {
+            const activities = res.data;
+            this.setState({ activities })
+        });
+    };
+
     render() {
         return (
             <div className="container">
                 <select
-                    name="type">
+                    name="type"
+                    onChange={event => this.changeStateValues(event.target)}>
                     <option value="">Select one</option>
                     <option value="Pushups">Pushups</option>
                     <option value="ABS">ABS</option>
                 </select>
-                {this.state.activities.map(activity => {
-                    return (
-                        <Activity
-                            onActivityDeleted={this.removeActivity.bind(this)}
-                            key={activity.id}
-                            type={activity.type}
-                            reps={activity.reps}
-                            totalTime={activity.totalTime}
-                            weight={activity.weight}
-                            id={activity.id}
-                        />
-                    );}
-                )}
+                <div className="card-columns">
+                    {this.state.activities.map(activity => {
+                        return (
+                            <Activity
+                                onActivityDeleted={this.removeActivity.bind(this)}
+                                key={activity.id}
+                                type={activity.type}
+                                reps={activity.reps}
+                                totalTime={activity.totalTime}
+                                weight={activity.weight}
+                                id={activity.id}
+                            />
+                        );}
+                    )}
+                </div>
             </div>
         );
     }
