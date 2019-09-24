6c345c850e3ac7808846eb73eade93c2e4eeb001
LolaEnBeta <lolitaenbeta@gmail.com>
2019-09-24 18:17:24 +0200

    Add style to ActivityForm with Bootsrap

--- src/components/ActivityForm.js
+++ src/components/ActivityForm.js
@@ -54,48 +54,60 @@ class ActivityForm extends React.Component {
 
     render() {
         return(
-            <div className="container">
+            <div className="card">
+                <h5 className="card-header info-color white-text text-center py-4">
+                    <strong>Create a new activity</strong>
+                </h5>
+                <div className="card-body mx-4 mt-4">
+                    <form
+                        onSubmit={this.addActivity.bind(this)}>
+                        <div className="md-form pb-1 pb-md-3">
+                                <select
+                                    name="type"
+                                    className="browser-default custom-select"
+                                    onChange={event => this.changeStateValues(event.target)}>
+                                    <option value="">Which activity did you do?</option>
+                                    <option value="Pushups">Pushups</option>
+                                    <option value="ABS">ABS</option>
+                                </select>
+                        </div>
 
-                <form
-                    onSubmit={this.addActivity.bind(this)}>
-
-                    <label>
-                        Which activity did you do?
-                        <select
-                            name="type"
-                            onChange={event => this.changeStateValues(event.target)}>
-                            <option value="">Select one</option>
-                            <option value="Pushups">Pushups</option>
-                            <option value="ABS">ABS</option>
-                        </select>
-                    </label>
-
-                    <label>
-                        How many repetitions did you do?
-                        <input
-                            type="text"
-                            name="reps"
-                            onChange={event => this.changeStateValues(event.target)}/>
-                    </label>
-
-                    <label>
-                            How many time did you work out?
+                        <div className="md-form pb-1 pb-md-3">
                             <input
                                 type="text"
-                                name="totalTime"
+                                className="form-control"
+                                name="reps"
+                                placeholder="How many repetitions did you do?"
                                 onChange={event => this.changeStateValues(event.target)}/>
-                    </label>
+                        </div>
 
-                    <label>
-                        How many weight did you use?
-                        <input
-                            type="text"
-                            name="weight"
-                            onChange={event => this.changeStateValues(event.target)}/>
-                    </label>
+                        <div className="md-form pb-1 pb-md-3">
+                            <input
+                                type="text"
+                                className="form-control"
+                                name="totalTime"
+                                placeholder="How many time did you work out?"
+                                onChange={event => this.changeStateValues(event.target)}/>
+                            </div>
 
-                    <input type="submit" value="ADD" />
-                </form>
+                        <div className="md-form pb-1 pb-md-3">
+                            <input
+                                type="text"
+                                className="form-control"
+                                name="weight"
+                                placeholder="How many weight did you use?"
+                                onChange={event => this.changeStateValues(event.target)}/>
+                        </div>
+
+                        <div className="row d-flex align-items-center mb-4">
+                            <div className="col-md-1 col-md-5 d-flex align-items-start">
+                                <div className="text-center">
+                                    <input className="btn btn-outline-info btn-rounded btn-block z-depth-0 my-4 waves-effect" type="submit" value="ADD" />
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         );
     }
